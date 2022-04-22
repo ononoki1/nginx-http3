a6d2ff2ae7bf598b8f357832d9051ef6f800c7e7
ononoki <me@ononoki.org>
2022-04-22 15:46:18 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,34 +1,40 @@
 set -e
-apt-get update
-apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy cmake dpkg-dev git golang libunwind-dev mercurial rsync wget unzip uuid-dev
-wget -O /etc/apt/trusted.gpg.d/nginx_signing.asc https://nginx.org/keys/nginx_signing.key
+cd /github/home
+echo 'Install dependencies.'
+apt-get update > /dev/null 2>&1
+apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy cmake dpkg-dev git golang libunwind-dev mercurial rsync wget unzip uuid-dev > /dev/null 2>&1
+wget -qO /etc/apt/trusted.gpg.d/nginx_signing.asc https://nginx.org/keys/nginx_signing.key
 echo -e 'deb https://nginx.org/packages/mainline/debian bullseye nginx\ndeb-src https://nginx.org/packages/mainline/debian bullseye nginx' >> /etc/apt/sources.list
 echo -e 'Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900' > /etc/apt/preferences.d/99nginx
-apt-get update
-apt-get build-dep --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy nginx
-cd /github/home
-apt-get source nginx
+apt-get update > /dev/null 2>&1
+apt-get build-dep --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy nginx > /dev/null 2>&1
+echo 'Fetch nginx and nginx-quic source code.'
+apt-get source nginx > /dev/null 2>&1
 mv nginx-* nginx
-hg clone -b quic https://hg.nginx.org/nginx-quic
-rsync -r nginx-quic/ nginx
+hg clone -b quic https://hg.nginx.org/nginx-quic > /dev/null 2>&1
+rsync -r nginx-quic/ nginx > /dev/null 2>&1
+echo 'Fetch boringssl source code.'
 cd nginx
-wget https://raw.githubusercontent.com/kn007/patch/master/Enable_BoringSSL_OCSP.patch
-patch -p1 < Enable_BoringSSL_OCSP.patch
+wget -q https://raw.githubusercontent.com/kn007/patch/master/Enable_BoringSSL_OCSP.patch
+patch -p1 < Enable_BoringSSL_OCSP.patch > /dev/null 2>&1
 mkdir debian/modules
 cd debian/modules
-git clone https://github.com/google/boringssl
+git clone https://github.com/google/boringssl > /dev/null 2>&1
+echo 'Build boringssl.'
 mkdir boringssl/build
 cd boringssl/build
-cmake ..
-make -j$(nproc)
+cmake .. > /dev/null 2>&1
+make -j$(nproc) > /dev/null 2>&1
+echo 'Fetch ngx_brotli and ngx_headers_more source code.'
 cd ../..
-git clone --recursive https://github.com/google/ngx_brotli
-git clone https://github.com/openresty/headers-more-nginx-module
+git clone --recursive https://github.com/google/ngx_brotli > /dev/null 2>&1
+git clone https://github.com/openresty/headers-more-nginx-module > /dev/null 2>&1
+echo 'Build target.'
 cd ..
 sed -i 's/CFLAGS=""/CFLAGS="-Wno-ignored-qualifiers"/g' rules
 sed -i 's/--sbin-path=\/usr\/sbin\/nginx/--sbin-path=\/usr\/sbin\/nginx --add-module="$(CURDIR)\/debian\/modules\/ngx_brotli" --add-module="$(CURDIR)\/debian\/modules\/headers-more-nginx-module"/g' rules
 sed -i 's/--with-cc-opt="$(CFLAGS)" --with-ld-opt="$(LDFLAGS)"/--with-http_v3_module --with-stream_quic_module --with-cc-opt="-I..\/modules\/boringssl\/include $(CFLAGS)" --with-ld-opt="-L..\/modules\/boringssl\/build\/ssl -L..\/modules\/boringssl\/build\/crypto $(LDFLAGS)"/g' rules
 cd ..
-dpkg-buildpackage -b
+dpkg-buildpackage -b > /dev/null 2>&1
 cd ..
 mv nginx_*.deb nginx.deb

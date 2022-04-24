7cdf8d0f4d34db785c48f49492c219e08eeac426
ononoki <me@ononoki.org>
2022-04-24 05:19:54 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -38,5 +38,5 @@ cd ..
 dpkg-buildpackage -b > /dev/null 2>&1
 cd ..
 mv nginx_*.deb nginx.deb
-hash=$(md5sum nginx.deb | awk '{print $1}')
-echo "hash=${hash:0:4}" >> $GITHUB_ENV
+hash=$(sha256sum nginx.deb | awk '{print $1}')
+echo "hash=${hash:0:7}" >> $GITHUB_ENV

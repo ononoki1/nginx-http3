84cccbc8b164c87a70d9bb895f0983dd38b423b8
ononoki <me@ononoki.org>
2022-04-26 10:58:19 +0800

    Add waf (#13)
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update README.md
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update README.md
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -e
 cd /github/home
 echo Install dependencies.
 apt-get update > /dev/null 2>&1
-apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy cmake curl dpkg-dev git golang libcurl4-openssl-dev libjemalloc-dev libpcre2-dev libunwind-dev libzstd-dev mercurial rsync wget > /dev/null 2>&1
+apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy cmake curl dpkg-dev git golang libcurl4-openssl-dev libjemalloc-dev libmodsecurity-dev libpcre2-dev libsodium-dev libunwind-dev libzstd-dev mercurial rsync wget > /dev/null 2>&1
 wget -qO /etc/apt/trusted.gpg.d/nginx_signing.asc https://nginx.org/keys/nginx_signing.key
 echo -e 'deb https://nginx.org/packages/mainline/debian bullseye nginx\ndeb-src https://nginx.org/packages/mainline/debian bullseye nginx' >> /etc/apt/sources.list
 echo -e 'Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900' > /etc/apt/preferences.d/99nginx
@@ -31,13 +31,18 @@ cd ../..
 git clone --recursive https://github.com/google/ngx_brotli > /dev/null 2>&1
 git clone https://github.com/openresty/headers-more-nginx-module > /dev/null 2>&1
 git clone https://github.com/tokers/zstd-nginx-module > /dev/null 2>&1
+git clone https://github.com/ononoki1/ngx_waf > /dev/null 2>&1
+cd ngx_waf
+git clone https://github.com/DaveGamble/cJSON lib/cjson > /dev/null 2>&1
+git clone https://github.com/troydhanson/uthash lib/uthash > /dev/null 2>&1
+cd ..
 git clone https://github.com/cloudflare/zlib > /dev/null 2>&1
 cd zlib
 make -f Makefile.in distclean > /dev/null 2>&1
 echo Build nginx.
 cd ../..
-sed -i 's/CFLAGS=""/CFLAGS="-Wno-ignored-qualifiers"/g' rules
-sed -i 's/--sbin-path=\/usr\/sbin\/nginx/--sbin-path=\/usr\/sbin\/nginx --add-module=$(CURDIR)\/debian\/modules\/ngx_brotli --add-module=$(CURDIR)\/debian\/modules\/headers-more-nginx-module --add-module=$(CURDIR)\/debian\/modules\/zstd-nginx-module/g' rules
+sed -i 's/CFLAGS=""/CFLAGS="-fstack-protector-strong -Wno-ignored-qualifiers -Wno-sign-compare"/g' rules
+sed -i 's/--sbin-path=\/usr\/sbin\/nginx/--sbin-path=\/usr\/sbin\/nginx --add-module=$(CURDIR)\/debian\/modules\/ngx_brotli --add-module=$(CURDIR)\/debian\/modules\/headers-more-nginx-module --add-module=$(CURDIR)\/debian\/modules\/zstd-nginx-module --add-module=$(CURDIR)\/debian\/modules\/ngx_waf/g' rules
 sed -i 's/--with-cc-opt="$(CFLAGS)" --with-ld-opt="$(LDFLAGS)"/--with-http_v3_module --with-stream_quic_module --with-zlib=$(CURDIR)\/debian\/modules\/zlib --with-cc-opt="-I..\/modules\/boringssl\/include $(CFLAGS)" --with-ld-opt="-ljemalloc -L..\/modules\/boringssl\/build\/ssl -L..\/modules\/boringssl\/build\/crypto $(LDFLAGS)"/g' rules
 cd ..
 dpkg-buildpackage -b > /dev/null 2>&1

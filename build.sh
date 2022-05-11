753f38d536d1200be0c86e6c6702f17bf2f2349d
ononoki <me@ononoki.org>
2022-05-11 04:29:23 +0000

    Change jemalloc (#17)
    * Update README.md
    
    * Update Dockerfile
    
    * Rename version to patch
    
    * Update build.yml
    
    * Update test.yml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -e
 cd /github/home
 echo Install dependencies.
 apt-get update > /dev/null 2>&1
-apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy cmake curl git golang libcurl4-openssl-dev libjemalloc-dev libmodsecurity-dev libsodium-dev libunwind-dev libzstd-dev mercurial ninja-build rsync wget > /dev/null 2>&1
+apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy cmake curl git golang libcurl4-openssl-dev libmodsecurity-dev libsodium-dev libunwind-dev libzstd-dev mercurial ninja-build rsync wget > /dev/null 2>&1
 wget -qO /etc/apt/trusted.gpg.d/nginx_signing.asc https://nginx.org/keys/nginx_signing.key
 echo deb-src http://nginx.org/packages/mainline/debian bullseye nginx >> /etc/apt/sources.list
 echo -e 'Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900' > /etc/apt/preferences.d/99nginx
@@ -36,6 +36,12 @@ git clone https://github.com/cloudflare/zlib > /dev/null 2>&1
 cd zlib
 make -f Makefile.in distclean > /dev/null 2>&1
 cd ..
+git clone https://github.com/jemalloc/jemalloc > /dev/null 2>&1
+cd jemalloc
+./autogen.sh > /dev/null 2>&1
+make > /dev/null 2>&1
+make install > /dev/null 2>&1
+cd ..
 git clone --recursive https://github.com/google/ngx_brotli > /dev/null 2>&1
 git clone https://github.com/openresty/headers-more-nginx-module > /dev/null 2>&1
 git clone https://github.com/GetPageSpeed/ngx_security_headers > /dev/null 2>&1
@@ -45,26 +51,27 @@ cd ..
 sed -i 's/CFLAGS=""/CFLAGS="-fstack-protector-strong -Wno-ignored-qualifiers -Wno-sign-compare"/g' rules
 sed -i 's/--sbin-path=\/usr\/sbin\/nginx/--sbin-path=\/usr\/sbin\/nginx --add-module=$(CURDIR)\/debian\/modules\/ngx_waf --add-module=$(CURDIR)\/debian\/modules\/ngx_brotli --add-module=$(CURDIR)\/debian\/modules\/headers-more-nginx-module --add-module=$(CURDIR)\/debian\/modules\/ngx_security_headers --add-module=$(CURDIR)\/debian\/modules\/zstd-nginx-module/g' rules
 sed -i 's/--with-cc-opt="$(CFLAGS)" --with-ld-opt="$(LDFLAGS)"/--with-http_v3_module --with-stream_quic_module --with-zlib=$(CURDIR)\/debian\/modules\/zlib --with-cc-opt="-I..\/modules\/boringssl\/include $(CFLAGS)" --with-ld-opt="-ljemalloc -L..\/modules\/boringssl\/build\/ssl -L..\/modules\/boringssl\/build\/crypto $(LDFLAGS)"/g' rules
+sed -i 's/dh_shlibdeps -a/dh_shlibdeps -a -- --ignore-missing-info/g' rules
 cd ..
 dpkg-buildpackage -b > /dev/null 2>&1
 cd ..
 mv nginx_*.deb nginx.deb
 hash=$(sha256sum nginx.deb | awk '{print $1}')
-version=$(cat /github/workspace/version)
+patch=$(cat /github/workspace/patch)
 minor=$(cat /github/workspace/minor)
 if [[ $hash != $(cat /github/workspace/hash) ]]; then
   echo $hash > /github/workspace/hash
   if [[ $GITHUB_EVENT_NAME == push ]]; then
-    version=0
+    patch=0
     minor=$(($(cat /github/workspace/minor)+1))
     echo $minor > /github/workspace/minor
   else
-    version=$(($(cat /github/workspace/version)+1))
+    patch=$(($(cat /github/workspace/version)+1))
   fi
-  echo $version > /github/workspace/version
+  echo $patch > /github/workspace/patch
   change=1
   echo This is a new version.
 else
   echo This is an old version.
 fi
-echo -e "hash=$hash\nversion=$version\nminor=$minor\nchange=$change" >> $GITHUB_ENV
+echo -e "hash=$hash\npatch=$patch\nminor=$minor\nchange=$change" >> $GITHUB_ENV

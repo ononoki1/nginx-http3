97ae5fbc86efa0cb2520287fb1f32489f71dd824
ononoki <me@ononoki.org>
2022-05-13 10:52:09 +0000

    Use latest curl directly from git

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -e
 cd /github/home
 echo Install dependencies.
 apt-get update > /dev/null 2>&1
-apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy cmake curl git golang libcurl4-openssl-dev libmaxminddb-dev libmodsecurity-dev libsodium-dev libunwind-dev libzstd-dev mercurial ninja-build rsync wget > /dev/null 2>&1
+apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy cmake curl git golang libmaxminddb-dev libmodsecurity-dev libsodium-dev libunwind-dev libzstd-dev mercurial ninja-build rsync wget > /dev/null 2>&1
 wget -qO /etc/apt/trusted.gpg.d/nginx_signing.asc https://nginx.org/keys/nginx_signing.key
 echo deb-src http://nginx.org/packages/mainline/debian bullseye nginx >> /etc/apt/sources.list
 echo -e 'Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900' > /etc/apt/preferences.d/99nginx
@@ -25,8 +25,17 @@ mkdir boringssl/build
 cd boringssl/build
 cmake -GNinja .. > /dev/null 2>&1
 ninja > /dev/null 2>&1
-echo Fetch jemalloc source code.
+echo Fetch curl source code.
 cd ../..
+git clone https://github.com/curl/curl > /dev/null 2>&1
+echo Build curl.
+cd curl
+autoreconf -fi
+./configure
+make -j$(nproc)
+make install
+echo Fetch jemalloc source code.
+cd ..
 git clone https://github.com/jemalloc/jemalloc > /dev/null 2>&1
 echo Build jemalloc.
 cd jemalloc

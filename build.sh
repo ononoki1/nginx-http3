a7b78bf8f34dc57a4e9184c943b919b3c9151480
ononoki <me@ononoki.org>
2022-05-16 04:37:09 +0000

    Revert to apt install

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -e
 cd /github/home
 echo Install dependencies.
 apt-get update > /dev/null 2>&1
-apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy cmake curl git golang libmaxminddb-dev libmodsecurity-dev libunwind-dev libzstd-dev mercurial ninja-build rsync wget > /dev/null 2>&1
+apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy cmake curl git golang libcurl4-openssl-dev libjemalloc-dev libmaxminddb-dev libmodsecurity-dev libsodium-dev libunwind-dev libzstd-dev mercurial ninja-build rsync wget > /dev/null 2>&1
 wget -qO /etc/apt/trusted.gpg.d/nginx_signing.asc https://nginx.org/keys/nginx_signing.key
 echo deb-src https://nginx.org/packages/mainline/debian bullseye nginx >> /etc/apt/sources.list
 echo -e 'Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900' > /etc/apt/preferences.d/99nginx
@@ -25,33 +25,8 @@ mkdir boringssl/build
 cd boringssl/build
 cmake -GNinja .. > /dev/null 2>&1
 ninja -j$(nproc) > /dev/null 2>&1
-echo Fetch curl source code.
-cd ../..
-git clone https://github.com/curl/curl > /dev/null 2>&1
-echo Build curl.
-cd curl
-autoreconf -fi > /dev/null 2>&1
-./configure --with-openssl > /dev/null 2>&1
-make -j$(nproc) > /dev/null 2>&1
-make install > /dev/null 2>&1
-echo Fetch jemalloc source code.
-cd ..
-git clone https://github.com/jemalloc/jemalloc > /dev/null 2>&1
-echo Build jemalloc.
-cd jemalloc
-./autogen.sh > /dev/null 2>&1
-make -j$(nproc) > /dev/null 2>&1
-make install > /dev/null 2>&1
-echo Fetch libsodium source code.
-cd ..
-git clone -b stable https://github.com/jedisct1/libsodium > /dev/null 2>&1
-echo Build libsodium.
-cd libsodium
-./configure > /dev/null 2>&1
-make -j$(nproc) > /dev/null 2>&1
-make install > /dev/null 2>&1
 echo Fetch additional dependencies.
-cd ..
+cd ../..
 git clone -b current https://github.com/ADD-SP/ngx_waf > /dev/null 2>&1
 cd ngx_waf
 git clone https://github.com/DaveGamble/cJSON lib/cjson > /dev/null 2>&1
@@ -71,7 +46,6 @@ cd ..
 sed -i 's/CFLAGS=""/CFLAGS="-fstack-protector-strong -Wno-ignored-qualifiers -Wno-sign-compare"/g' rules
 sed -i 's/--sbin-path=\/usr\/sbin\/nginx/--sbin-path=\/usr\/sbin\/nginx --add-module=$(CURDIR)\/debian\/modules\/ngx_waf --add-module=$(CURDIR)\/debian\/modules\/ngx_brotli --add-module=$(CURDIR)\/debian\/modules\/ngx_http_geoip2_module --add-module=$(CURDIR)\/debian\/modules\/headers-more-nginx-module --add-module=$(CURDIR)\/debian\/modules\/ngx_security_headers --add-module=$(CURDIR)\/debian\/modules\/zstd-nginx-module/g' rules
 sed -i 's/--with-cc-opt="$(CFLAGS)" --with-ld-opt="$(LDFLAGS)"/--with-http_v3_module --with-stream_quic_module --with-zlib=$(CURDIR)\/debian\/modules\/zlib --with-cc-opt="-I..\/modules\/boringssl\/include $(CFLAGS)" --with-ld-opt="-ljemalloc -L..\/modules\/boringssl\/build\/ssl -L..\/modules\/boringssl\/build\/crypto $(LDFLAGS)"/g' rules
-sed -i 's/dh_shlibdeps -a/dh_shlibdeps -a -- --ignore-missing-info/g' rules
 cd ..
 dpkg-buildpackage -b > /dev/null 2>&1
 cd ..

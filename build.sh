4f84e9348d18139c7967b44729f997ad5c97b154
ononoki <me@ononoki.org>
2022-06-24 05:07:51 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,19 +2,27 @@ set -e
 cd /github/home
 echo Install dependencies.
 apt-get update > /dev/null 2>&1
-apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy cmake curl git golang libcurl4-openssl-dev libjemalloc-dev libmaxminddb-dev libmodsecurity-dev libsodium-dev libunwind-dev mercurial ninja-build rsync wget > /dev/null 2>&1
+apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential \
+-o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy \
+cmake curl git golang libjemalloc-dev libmaxminddb-dev libunwind-dev mercurial \
+ninja-build rsync wget > /dev/null 2>&1
 wget -qO /etc/apt/trusted.gpg.d/nginx_signing.asc https://nginx.org/keys/nginx_signing.key
-echo deb-src https://nginx.org/packages/mainline/debian bullseye nginx >> /etc/apt/sources.list
-echo -e 'Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900' > /etc/apt/preferences.d/99nginx
+echo deb-src https://nginx.org/packages/mainline/debian bullseye nginx \
+>> /etc/apt/sources.list
+echo -e 'Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900' \
+> /etc/apt/preferences.d/99nginx
 apt-get update > /dev/null 2>&1
-apt-get build-dep --allow-change-held-packages --allow-downgrades --allow-remove-essential -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy nginx > /dev/null 2>&1
+apt-get build-dep --allow-change-held-packages --allow-downgrades --allow-remove-essential \
+-o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy \
+nginx > /dev/null 2>&1
 echo Fetch nginx and nginx-quic source code.
 apt-get source nginx > /dev/null 2>&1
 mv nginx-* nginx
 hg clone -b quic https://hg.nginx.org/nginx-quic > /dev/null 2>&1
 rsync -r nginx-quic/ nginx > /dev/null 2>&1
 cd nginx
-curl -s https://raw.githubusercontent.com/kn007/patch/master/Enable_BoringSSL_OCSP.patch | patch -p1 > /dev/null 2>&1
+curl -s https://raw.githubusercontent.com/kn007/patch/master/Enable_BoringSSL_OCSP.patch \
+| patch -p1 > /dev/null 2>&1
 echo Fetch boringssl source code.
 mkdir debian/modules
 cd debian/modules
@@ -26,11 +34,6 @@ cmake -GNinja .. > /dev/null 2>&1
 ninja -j$(nproc) > /dev/null 2>&1
 echo Fetch additional dependencies.
 cd ../..
-git clone -b current https://github.com/ADD-SP/ngx_waf > /dev/null 2>&1
-cd ngx_waf
-git clone https://github.com/DaveGamble/cJSON lib/cjson > /dev/null 2>&1
-git clone https://github.com/troydhanson/uthash lib/uthash > /dev/null 2>&1
-cd ..
 git clone https://github.com/cloudflare/zlib > /dev/null 2>&1
 cd zlib
 make -f Makefile.in distclean > /dev/null 2>&1
@@ -40,8 +43,8 @@ git clone https://github.com/openresty/headers-more-nginx-module > /dev/null 2>&
 git clone https://github.com/leev/ngx_http_geoip2_module > /dev/null 2>&1
 echo Build nginx.
 cd ..
-sed -i 's/CFLAGS=""/CFLAGS="-fstack-protector-strong -Wno-ignored-qualifiers -Wno-sign-compare"/g' rules
-sed -i 's/--sbin-path=\/usr\/sbin\/nginx/--sbin-path=\/usr\/sbin\/nginx --add-module=$(CURDIR)\/debian\/modules\/ngx_waf --add-module=$(CURDIR)\/debian\/modules\/ngx_brotli --add-module=$(CURDIR)\/debian\/modules\/headers-more-nginx-module --add-module=$(CURDIR)\/debian\/modules\/ngx_http_geoip2_module/g' rules
+sed -i 's/CFLAGS=""/CFLAGS="-Wno-ignored-qualifiers"/g' rules
+sed -i 's/--sbin-path=\/usr\/sbin\/nginx/--sbin-path=\/usr\/sbin\/nginx --add-module=$(CURDIR)\/debian\/modules\/ngx_brotli --add-module=$(CURDIR)\/debian\/modules\/headers-more-nginx-module --add-module=$(CURDIR)\/debian\/modules\/ngx_http_geoip2_module/g' rules
 sed -i 's/--with-cc-opt="$(CFLAGS)" --with-ld-opt="$(LDFLAGS)"/--with-zlib=$(CURDIR)\/debian\/modules\/zlib --with-cc-opt="-I..\/modules\/boringssl\/include $(CFLAGS)" --with-ld-opt="-ljemalloc -L..\/modules\/boringssl\/build\/ssl -L..\/modules\/boringssl\/build\/crypto $(LDFLAGS)"/g' rules
 sed -i 's/--http-uwsgi-temp-path=\/var\/cache\/nginx\/uwsgi_temp --http-scgi-temp-path=\/var\/cache\/nginx\/scgi_temp //g' rules
 sed -i 's/--with-compat //g' rules

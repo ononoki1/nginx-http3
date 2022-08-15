2e42adbe07164fd1b9a1170048a248789a0b9bca
ononoki <me@ononoki.org>
2022-08-15 17:22:40 +0000

    Fix SSL cert verify error

--- build.sh
+++ build.sh
@@ -1,24 +1,27 @@
 set -e
 export APT_LISTCHANGES_FRONTEND=none
 export DEBIAN_FRONTEND=noninteractive
-echo -e 'deb https://deb.debian.org/debian bookworm main contrib non-free\ndeb https://deb.debian.org/debian-security bookworm-security main contrib non-free\ndeb https://deb.debian.org/debian bookworm-updates main contrib non-free\ndeb-src https://nginx.org/packages/mainline/debian bullseye nginx' \
-> /etc/apt/sources.list
 cd /github/home
 echo Install dependencies.
+apt-get update
+apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential \
+-o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy \
+ca-certificates wget
+wget -qO /etc/apt/trusted.gpg.d/nginx_signing.asc https://nginx.org/keys/nginx_signing.key
+echo -e 'deb https://deb.debian.org/debian bookworm main contrib non-free\ndeb https://deb.debian.org/debian-security bookworm-security main contrib non-free\ndeb https://deb.debian.org/debian bookworm-updates main contrib non-free\ndeb-src https://nginx.org/packages/mainline/debian bullseye nginx' \
+> /etc/apt/sources.list
+echo -e 'Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900' \
+> /etc/apt/preferences.d/99nginx
 apt-get update
 apt-get dist-upgrade --allow-change-held-packages --allow-downgrades --allow-remove-essential \
 -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy
 apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential \
 -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy \
-cmake curl git golang libmaxminddb-dev mercurial ninja-build rsync wget zlib1g-dev \
+cmake curl git golang libmaxminddb-dev mercurial ninja-build rsync zlib1g-dev \
 > /dev/null 2>&1
-wget -qO /etc/apt/trusted.gpg.d/nginx_signing.asc https://nginx.org/keys/nginx_signing.key
-echo -e 'Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900' \
-> /etc/apt/preferences.d/99nginx
-apt-get update > /dev/null 2>&1
 apt-get build-dep --allow-change-held-packages --allow-downgrades --allow-remove-essential \
 -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy \
-nginx > /dev/null 2>&1
+nginx
 echo Fetch nginx and nginx-quic source code.
 apt-get source nginx > /dev/null 2>&1
 mv nginx-* nginx

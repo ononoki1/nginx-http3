ac5ab389ba133ec5e9486dff9bca64e85fd178d6
xuechen <me@ononoki.org>
2024-02-22 14:05:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo deb http://deb.debian.org/debian bullseye-backports main >> /etc/apt/source
 apt-get update > /dev/null 2>&1
 apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential \
 -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy \
-cmake curl git libmaxminddb-dev wget zlib1g-dev > /dev/null 2>&1
+cmake git libmaxminddb-dev wget > /dev/null 2>&1
 wget -qO /etc/apt/trusted.gpg.d/nginx_signing.asc https://nginx.org/keys/nginx_signing.key
 echo deb-src https://nginx.org/packages/mainline/debian bullseye nginx \
 >> /etc/apt/sources.list
